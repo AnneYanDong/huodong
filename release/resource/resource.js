14db89bb9db2eea1f895aded7398e505d52c9e46
陈朝霞 <chenzhaoxia@jianbing.com>
2017-06-16 18:56:53 +0800

    提交

--- release/resource/resource.js
+++ release/resource/resource.js
@@ -71,7 +71,7 @@
 
 	"20170601": {
 		"js": ["r.51gjj.com/act/release/js/20170601_397b14d.js"],
-		"css": ["r.51gjj.com/act/release/css/20170601_51deff0.css"]
+		"css": ["r.51gjj.com/act/release/css/20170601_a31e9d6.css"]
 	},
 
 	"20170605": {
