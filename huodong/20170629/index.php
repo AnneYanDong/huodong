d24afb94cd2e41f5a385f9da7b9d9fa1a83ba004
yandong <yandong@jianbing.com>
2017-06-30 16:19:06 +0800

    宜人贷-20170629

---
+++ huodong/20170629/index.php
@@ -0,0 +1,50 @@
+<?php
+    $title = "宜人贷"; // 配置标题的，必须
+    include "../public/v1/header.php";
+?>
+<?php
+    $actDate = "20170629"; // 配置图片的，必须
+    $obj = new Resource; // 实例化获取资源类
+    $res = $obj->getResStr($actDate); // 获取指定的活动资源，返回数组，array("css"=> "..", "js"=>"。。")
+    $origin = $_SERVER['HTTP_HOST'];
+    if (preg_match('/b\.jianbing\.com/', $origin) || preg_match('/kaifa\.jianbing\.com/', $origin)) {
+        $imgUrl = '//r.51gjj.com/act/release/img/' . $actDate . '_';
+    } else {
+        $imgUrl = '../static/img/' . $actDate . '_';
+    };
+?>
+    <link rel="stylesheet" href="<?php echo $res['css'] ?>">
+    <style>
+        /* 加载动画颜色配置 */
+        .loading-bg-color {
+            background: #001356;
+        }
+
+        .loading-ele-color:after, .loading-ele-color:before {
+            background: #001356 !important;
+        }
+    </style>
+     <script>
+        //加载test.php假接口
+        var indexData = {
+            ajaxUrl: "<?php echo 'test.php'; ?>"
+        };
+    </script>
+    <script>
+       var projectName = "<?php echo $actDate ?>";
+    </script>
+    <!-- 引入require.js和设置文件js的入口 -->
+    <script src="../static/js/lib/require.min.js" data-main="<?php echo $res['js'] ?>"></script>
+    </head>
+    <body>
+        <div class="wp hide">
+            <div class="wp-inner">
+                <div class="content">
+                    <img data-src="<?php echo $imgUrl; ?>yrd_bg.jpg" alt="bg" />
+                    <div class="btn" bp="马上领取" title="马上领取">马上领取</div>
+                </div>
+            <div class="mask hide"></div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ huodong/static/js/20170629.js
@@ -0,0 +1,179 @@
+//引入配置对象
+require.config(requireConfig);
+require(["jquery", "fastClick", "FullPage", "ct", "bridge", "juicer"], function ($, fastClick, fullpage, ct, Bridge, juicer) {
+    var oMask = $(".mask");
+    /*oP和oM的作用是在body元素的最下方默认生成两个div，一个是提示框，一个是遮罩层*/
+    var oP = Object.create(ct.Prompt); //提示框对象
+    oP.create().build();
+
+    var oM = Object.create(ct.Mask); //遮罩层
+    oM.create().build();
+
+    var local = ct.Tool.local(); //获取域名
+
+    ct.Tool.buryPoint(); //统计点击情况
+
+    var run = {
+        start: function () {
+            var _this = this;
+
+            /*解决移动端click点击300延迟*/
+            fastClick.attach(document.body);
+
+            /*设置HTML的font-size*/
+            ct.Tool.setFont();
+            //这个是给content自动加上固定宽高
+            ct.Tool.handleBottomStatusBar();
+            window.addEventListener("resize", ct.Tool.debounce(ct.Tool.setFont));
+            window.addEventListener("resize", ct.Tool.debounce(ct.Tool.handleBottomStatusBar));
+
+
+            /*整体预加载动画*/
+            var oPreLoading = Object.create(ct.PreLodingUi);
+            oPreLoading.create({
+                preLoadingCls: "loading-bg-color", // 自定义加载动画颜色
+                loadingEleCls: "loading-ele-color"
+            }).build();
+            var timer = null;
+            clearTimeout(timer);
+            timer = setTimeout(function () {
+                oPreLoading.hide();
+                // _this.init();
+            }, 500);
+            /*图片预加载*/
+            ct.Tool.imgPreLoad({
+                callback: function () {
+                    this.hintLog("图片加载完成");
+                    var timer = null;
+                    clearTimeout(timer);
+                    timer = setTimeout(function () {
+                        oPreLoading.hide();
+                        _this.init();
+                    }, 500)
+                }
+            })
+        },
+
+
+        init: function () {
+            var _this = this;
+            $('.wp').removeClass('hide');
+            console.log("宜人贷活动！");
+
+            _this.receive();
+        },
+         receive: function() {
+            $('.content').on('click','.btn',function(event){
+
+                //这个是模拟的假请求
+                // ct.Ajax.do({
+                //     url: indexData.ajaxUrl || "test.php",  //用test.php来模拟接口
+                //     data: {},
+                //     success: function (d) {
+                //         console.log(d);
+                //         //判断用户有没有领取补贴
+                //         if (d.success == true) {
+                //             var timer = null;
+                //             clearTimeout(timer);
+                //             if (d.ret.weChat == true) {
+                //                 timer = setTimeout(function () {
+                //                     oP.show("登录51公积金管家APP领取");
+                //                     timer = setTimeout(function () {
+                //                         window.location.href = d.ret.url;
+                //                     }, 1500);
+                //                 }, 200);
+                //             } else {
+                //                 if (d.ret.qq == true) {
+                //                     timer = setTimeout(function () {
+                //                         oP.show("登录51公积金管家APP领取");
+                //                         timer = setTimeout(function () {
+                //                             window.location.href = d.ret.url;
+                //                         }, 1500);
+                //                     }, 200);
+                //                 } else {
+                //                     if (d.ret.login == false) {
+                //                         if (Bridge) {
+                //                             Bridge.action("login");
+                //                         }
+                //                     } else {
+                //                         if (d.ret.order == false) {
+                //                             oP.show("您暂不符合活动参与条件,试试其它活动~");
+                //                         } else {
+                //                             timer = setTimeout(function () {
+                //                                 timer = setTimeout(function () {
+                //                                     window.location.href = d.ret.url;
+                //                                 }, 1500)
+                //                             }, 200);
+                //                         }
+                //                     }
+                //                 }
+                //             }
+                //         }else {
+                //             oP.show(d.msg || "出错了请重试");
+                //         }
+                //     }
+                // });
+
+
+                /*下面这个是真正的请求真接口，别删*/
+                $.ajax({
+                    type: "POST",
+                    dataType: "JSON",
+                    // url: ct.Tool.url("/act/act170629/get_status"),
+                    url: "/act/act170629/get_status",  //这个到时候换成鑫福贷活动的接口
+                    data: JSON.stringify({
+                        place_cid: ct.Tool.userAgent().isGjj ? 1 : 0,
+                        tag: "进入页面" + projectName
+                    }),
+                    success: function (d) {
+                        console.log(d);
+                        if (d.success == true) {
+                            var timer = null;
+                            clearTimeout(timer);
+                            if (d.ret.weChat == true) {
+                                timer = setTimeout(function () {
+                                    oP.show("登录51公积金管家APP领取奖品");
+                                    timer = setTimeout(function () {
+                                        window.location.href = d.ret.url;
+                                    }, 1500);
+                                }, 200);
+                            } else {
+                                if (d.ret.qq == true) {
+                                    console.log('qq');
+                                    timer = setTimeout(function () {
+                                        oP.show("登录51公积金管家APP领取奖品");
+                                        timer = setTimeout(function () {
+                                            window.location.href = d.ret.url;
+                                        }, 1500);
+                                    }, 200);
+                                } else {
+                                    if (d.ret.login == false) {
+                                        if (Bridge) {
+                                            Bridge.action("login");
+                                        }
+                                    } else {
+                                        if (d.ret.order == false) {
+                                            oP.show("您暂不符合活动参与条件,试试其它活动~");
+                                        } else {
+                                            timer = setTimeout(function () {
+                                                timer = setTimeout(function () {
+                                                    window.location.href = d.ret.url;
+                                                }, 1500)
+                                            }, 200);
+                                        }
+                                    }
+                                }
+                            }
+                        }else {
+                            oP.show(d.msg || "出错请重试");
+                        }
+                    }
+                });
+            });
+        },
+
+    }
+
+    run.start();
+
+});
