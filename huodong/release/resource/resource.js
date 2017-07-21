6f6d15e338a5ecba486374a90fbe05f26adcf589
yandong <anne_yan@foxmail.com>
2017-07-21 16:30:48 +0800

    改下接口

--- huodong/release/resource/resource.js
+++ huodong/release/resource/resource.js
@@ -40,7 +40,7 @@
 	},
 
 	"20170512": {
-		"js": ["r.51gjj.com/act/release/js/20170512_5cc3457.js"],
+		"js": ["r.51gjj.com/act/release/js/20170512_9d4c6a4.js"],
 		"css": ["r.51gjj.com/act/release/css/20170512_9cdc335.css"]
 	},
 
