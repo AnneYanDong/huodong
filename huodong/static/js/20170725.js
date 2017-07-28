c0e968300a891be3853f7a1390e50436a310bc21
陈朝霞 <chenzhaoxia@jianbing.com>
2017-07-28 18:00:23 +0800

    提交

--- huodong/static/js/20170725.js
+++ huodong/static/js/20170725.js
@@ -269,7 +269,9 @@ require(["jquery", "fastClick", "lucky-card", "ct", "bridge", "juicer", "marquee
             _this.getCustLabel();
             $(".atm-content").on("click",".withdraw-btn",function(){
                 console.log(purpose);
-                $(".withdraw-btn").attr("disabled",true);
+                $(".withdraw-btn").attr({
+                        "disabled": "disabled"
+                    });
                 $(".atm-content .finger-box").fadeOut();
                 var amount = $(".atm-total-input input").val();
                 $.ajax({
@@ -281,7 +283,7 @@ require(["jquery", "fastClick", "lucky-card", "ct", "bridge", "juicer", "marquee
                     success: function(d){
                         console.log("请求数据->",d);
                         if (d.success) {
-                            $(".withdraw-btn").attr("disabled",false);
+                            $(".withdraw-btn").removeAttr("disabled");
                             var d = d.ret;
                             if(d.is_weChat || d.is_qq) {
                                 console.log("微信或qq");
