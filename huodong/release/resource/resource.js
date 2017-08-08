297e67129ee4a38496cdafae7c3930e1e3134e72
yandong <anne_yan@foxmail.com>
2017-08-08 13:44:29 +0800

    0801加页面埋点

--- huodong/release/resource/resource.js
+++ huodong/release/resource/resource.js
@@ -160,7 +160,7 @@
 	},
 
 	"20170801": {
-		"js": ["r.51gjj.com/act/release/js/20170801_752e950.js"],
+		"js": ["r.51gjj.com/act/release/js/20170801_47bd816.js"],
 		"css": ["r.51gjj.com/act/release/css/20170801_4891842.css"]
 	},
 
