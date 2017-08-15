59efa29cd4fcae2bc9c53588c1732ec147cec9b1
yandong <anne_yan@foxmail.com>
2017-08-15 20:48:29 +0800

    0807

--- huodong/release/resource/resource.js
+++ huodong/release/resource/resource.js
@@ -165,7 +165,7 @@
 	},
 
 	"20170807": {
-		"js": ["r.51gjj.com/act/release/js/20170807_acc6100.js"],
-		"css": ["r.51gjj.com/act/release/css/20170807_4e19fc0.css"]
+		"js": ["r.51gjj.com/act/release/js/20170807_f4d99c5.js"],
+		"css": ["r.51gjj.com/act/release/css/20170807_780dcca.css"]
 	}
 }
