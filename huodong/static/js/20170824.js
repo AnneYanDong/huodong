75539e33398dd26e554e110589d4fb6d32a868c1
yandong <anne_yan@foxmail.com>
2017-08-24 17:40:04 +0800

    0824

--- huodong/static/js/20170824.js
+++ huodong/static/js/20170824.js
@@ -65,11 +65,13 @@ require(["jquery", "fastClick", "FullPage", "ct", "bridge", "juicer"], function 
                 success: function (d) {
                     console.log("后台数据：",d);
                     if (10100 <= d.resCode < 10200) {
+                        console.log("10100-10200");
                         if(Bridge) {
                             Bridge.action("login");
                         }
                     } else {
                         if (d.resCode == 1) {
+                            console.log("1");
                             $(".amount span:eq(1)").text(d.resData.currentUserTenderMoney);
                         } else {
                             oP.show(d.resMsg || "出错请重试");
