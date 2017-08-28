a683c381fa84bc357c246dbaab89ea9eaeec4e20
陈朝霞 <chenzhaoxia@jianbing.com>
2017-08-28 10:56:52 +0800

    提交

--- huodong/public/v1/header.php
+++ huodong/public/v1/header.php
@@ -69,6 +69,9 @@
                 },
                 "number":{
                     "deps": ["jquery"]
+                },
+                "share": {
+                    "exports": "share",
                 }
             }
         }
