b75da4f08aec7582052b0a301f7a2bef9606a6b5
yandong <anne_yan@foxmail.com>
2017-09-11 13:56:46 +0800

    16条Lp&&0807新增优化要求

--- huodong/public/v2/header.php
+++ huodong/public/v2/header.php
@@ -13,7 +13,8 @@
             "baseUrl": "../../",
             "paths": {
                 "jquery": ["//r.51gjj.com/act/release/js/lib/jQuery.min", "lib/jQuery.min"],
-                "zepto": ["//r.51gjj.com/act/release/js/lib/zepto.min", "lib/zepto.min"],
+                "zepto": ["//r.51gjj.com/act/release/js/lib/pageSlider.min", "lib/pageSlider.min"],
+                "pageSlider": ["//r.51gjj.com/act/release/js/lib/zepto.min", "lib/zepto.min"],
                 "bridge": ["//r.51gjj.com/act/release/js/lib/bridge", "lib/bridge"],
                 "FullPage": ["//r.51gjj.com/act/release/js/lib/js.fullpage", "lib/js.fullpage"],
                 "qrcode": ["//r.51gjj.com/act/release/js/lib/qrcode.min", "lib/qrcode.min"], //二维码生成
