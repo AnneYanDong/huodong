c427466b677d441aa1c4cd756c070379651df863
陈朝霞 <chenzhaoxia@jianbing.com>
2017-09-11 19:10:03 +0800

    理财活动

---
+++ huodong/20170909/test.php
@@ -0,0 +1,11 @@
+<?php 
+
+	$resData = array(
+        "resCode"=>1,
+        "resMsg"=>"操作成功",
+        "resData"=> array(
+            "type"=>5
+        )
+	);
+	echo json_encode($resData);
+ ?>
---
+++ huodong/release/resource/20170908-conf.js
@@ -0,0 +1,6 @@
+{
+    "v1": {
+      "js": ["r.51gjj.com/act/release/js/20170908_9d94a19.js"],
+      "css": ["r.51gjj.com/act/release/css/20170908_f86c478.css"]
+    }
+}
