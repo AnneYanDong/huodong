312f45d703bd36c2e2d74984555e7a720565bb4f
陈朝霞 <chenzhaoxia@jianbing.com>
2017-09-12 16:32:31 +0800

    提交

---
+++ huodong/release/resource/20170908_invest-conf.js
@@ -0,0 +1,6 @@
+{
+    "v1": {
+      "js": ["r.51gjj.com/act/release/js/20170908_invest_2965681.js"],
+      "css": ["r.51gjj.com/act/release/css/20170908_invest_42b7b1d.css"]
+    }
+}
---
+++ huodong/release/resource/20170909-conf.js
@@ -0,0 +1,6 @@
+{
+    "v1": {
+      "js": ["r.51gjj.com/act/release/js/20170909_2543329.js"],
+      "css": ["r.51gjj.com/act/release/css/20170909_973aa52.css"]
+    }
+}
