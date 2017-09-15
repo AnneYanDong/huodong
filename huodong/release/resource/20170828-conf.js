e3cb7e886a6f62acc12663df9e4bfeed705520df
yandong <anne_yan@foxmail.com>
2017-09-15 12:55:33 +0800

    all

--- huodong/release/resource/20170828-conf.js
+++ huodong/release/resource/20170828-conf.js
@@ -1,6 +1,6 @@
 {
   "v1": {
-    "js": ["r.51gjj.com/act/release/js/20170828_b2263f1.js"],
+    "js": ["r.51gjj.com/act/release/js/20170828_1d95aa8.js"],
     "css": ["r.51gjj.com/act/release/css/20170828_6b78680.css"]
   }
 }
