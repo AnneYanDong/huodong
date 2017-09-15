9bc9fa2f0f0519c5355afc5e3c3bca6f9a4c7991
陈朝霞 <chenzhaoxia@jianbing.com>
2017-09-15 13:19:04 +0800

    修改

--- release/resource/20170909-conf.js
+++ release/resource/20170909-conf.js
@@ -1,6 +1,6 @@
 {
     "v1": {
       "js": ["r.51gjj.com/act/release/js/20170909_b9a4a0d.js"],
-      "css": ["r.51gjj.com/act/release/css/20170909_e9772c3.css"]
+      "css": ["r.51gjj.com/act/release/css/20170909_309fc8d.css"]
     }
 }
---
+++ release/resource/20170913-conf.js
@@ -0,0 +1,6 @@
+{
+    "v1": {
+      "js": ["r.51gjj.com/act/release/js/20170913_13687e8.js"],
+      "css": ["../css/20170913.css"]
+    }
+}
