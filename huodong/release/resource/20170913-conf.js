a68cb447206f37f84d481e36b400ad72477496b8
c690554125 <690554125@qq.com>
2017-09-21 11:37:07 +0800

    913313

--- huodong/release/resource/20170913-conf.js
+++ huodong/release/resource/20170913-conf.js
@@ -1,6 +1,6 @@
 {
     "v1": {
-      "js": ["r.51gjj.com/act/release/js/20170913_8c1d959.js"],
+      "js": ["r.51gjj.com/act/release/js/20170913_7e14048.js"],
       "css": ["r.51gjj.com/act/release/css/20170913_2990632.css?v=asfsfsfww221sr"]
     }
 }
