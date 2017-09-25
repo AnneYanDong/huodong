85dcb46c4064c7a4c0a78b765318fa3bfe94d8fb
陈朝霞 <chenzhaoxia@jianbing.com>
2017-09-25 16:51:28 +0800

    提交

--- huodong/static/js/20170919.js
+++ huodong/static/js/20170919.js
@@ -65,7 +65,7 @@ require(["jquery", "fastClick", "lucky-card", "ct", "bridge", "juicer", "marquee
                 dataType: "JSON",
                 url: ct.Tool.url("/app/request/activity"),
                 data: JSON.stringify({
-                    place_cid: ct.Tool.userAgent().isGjj ? 1 : 0,
+                    source: ct.Tool.userAgent().isGjj ? 1 : 0,
                     tag: "20170919_1_0_0_限时加息"
                 }),
                 success: function (d) {
