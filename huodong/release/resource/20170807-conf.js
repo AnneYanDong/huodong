e4377a34c6e5ffd36633f5d8e87f335a4dcec91f
yandong <anne_yan@foxmail.com>
2017-09-25 16:52:11 +0800

    ll

--- huodong/release/resource/20170807-conf.js
+++ huodong/release/resource/20170807-conf.js
@@ -1,6 +1,6 @@
 {
   "v1": {
-    "js": ["r.51gjj.com/act/release/js/20170807_50ddccf.js"],
+    "js": ["r.51gjj.com/act/release/js/20170807_9ec652c.js"],
     "css": ["r.51gjj.com/act/release/css/20170807_40e409e.css"]
   }
 }
--- huodong/release/resource/resource.js
+++ huodong/release/resource/resource.js
@@ -165,7 +165,7 @@
 	},
 
 	"20170807": {
-		"js": ["r.51gjj.com/act/release/js/20170807_50ddccf.js"],
+		"js": ["r.51gjj.com/act/release/js/20170807_9ec652c.js"],
 		"css": ["r.51gjj.com/act/release/css/20170807_40e409e.css"]
 	}
 }
