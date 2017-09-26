8555cf13dd386edb6a694f1fa89da56dc203d7ae
c690554125 <690554125@qq.com>
2017-09-26 17:29:23 +0800

    市场活动2个页面

--- huodong/static/resource/20170925invitation-conf.js
+++ huodong/static/resource/20170925invitation-conf.js
@@ -2,5 +2,9 @@
     "regist": {
       "js": [__uri("../js/20170925invitation/regist.js")],
       "css": [__uri("../css/20170925invitation/regist.css")]
+    },
+    "appdown": {
+    	"js": [__uri("../js/20170925invitation/appdown.js")],
+      "css": [__uri("../css/20170925invitation/appdown.css")]
     }
 }
