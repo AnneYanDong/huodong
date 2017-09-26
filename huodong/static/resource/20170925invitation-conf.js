ee0bad17a7d5a88dfe245781cc8be10ec7e04517
c690554125 <690554125@qq.com>
2017-09-26 10:35:46 +0800

    '改动下结构，未完成'

--- huodong/static/resource/20170925invitation-conf.js
+++ huodong/static/resource/20170925invitation-conf.js
@@ -2,13 +2,5 @@
     "regist": {
       "js": [__uri("../js/20170925invitation/regist.js")],
       "css": [__uri("../css/20170925invitation/regist.css")]
-    },
-    "myinvitation": {
-      "js": [__uri("../js/20170925invitation/myinvitation.js")],
-      "css": [__uri("../css/20170925invitation/myinvitation.css")]
-    },
-    "invitation": {
-      "js": [__uri("../js/20170925invitation/invitation.js")],
-      "css": [__uri("../css/20170925invitation/invitation.css")]
     }
 }
