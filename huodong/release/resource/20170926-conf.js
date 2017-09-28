e2aa6ae3d45713ad8502102188628513ea9511a4
陈朝霞 <chenzhaoxia@jianbing.com>
2017-09-28 13:52:08 +0800

    提交

--- huodong/release/resource/20170926-conf.js
+++ huodong/release/resource/20170926-conf.js
@@ -1,6 +1,6 @@
 {
     "v1": {
       "js": ["r.51gjj.com/act/release/js/20170926_b6eb615.js"],
-      "css": ["r.51gjj.com/act/release/css/20170926_32616d0.css"]
+      "css": ["r.51gjj.com/act/release/css/20170926_e6db61c.css"]
     }
 }
