fa9bc3186a6c07009a07ee21f5d94f94ba0e83f3
c690554125 <690554125@qq.com>
2017-10-10 19:30:54 +0800

    jsjs

--- huodong/release/resource/20170918-conf.js
+++ huodong/release/resource/20170918-conf.js
@@ -1,6 +1,6 @@
 {
     "v1": {
       "js": ["r.51gjj.com/act/release/js/20170918_a70029a.js"],
-      "css": ["r.51gjj.com/act/release/css/20170918_bf779db.css"]
+      "css": ["r.51gjj.com/act/release/css/20170918_414328b.css"]
     }
 }
